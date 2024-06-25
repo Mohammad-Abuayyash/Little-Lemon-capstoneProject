ba430f73952deebdb28c2470715a48a639bdf833
BmoxX <mohabuayyash2002@gmail.com>
2024-06-25 05:20:26 +0300

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './components/Header/index';
+import CallToAction from './components/CallToAction/index';
+import Specials from './components/Specials/index';
 import Footer from './components/Footer/index';
 function App() {
   return (
     <>
      <Header/>
+      <CallToAction />
+      <Specials />
+      {/*<CustomersReveiws />
+      <Chicago /> */}
      <Footer/>
     </>
   );
---
+++ src/components/CallToAction/index.js
@@ -0,0 +1,24 @@
+import "../CallToAction/style.css"
+import RestaFood from "../../assets/restauranfood.jpg"
+const CallToAction = () => {
+
+ return (
+    <section className="callToAction">
+        <div className="details">
+            <h1>Little Lemon</h1>
+            <h3>Chicago</h3>
+            <p>We are a family owned 
+                Mediterranean restaurant,
+                focused on traditional
+                recipes served with a modern
+                twist.
+            </p>
+            <button type="button">Reserve a table</button>
+        </div>
+        <img src={RestaFood}></img>
+    </section>
+ );
+
+}
+
+export default CallToAction;
