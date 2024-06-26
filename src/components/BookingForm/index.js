fbec54c6aab7ccd7d13118be3441ec39f4c53624
BmoxX <mohabuayyash2002@gmail.com>
2024-06-26 12:50:08 +0300

    8th commit

--- src/components/BookingForm/index.js
+++ src/components/BookingForm/index.js
@@ -1,24 +1,28 @@
 import React from "react";
 import "./style.css"
 
+
 const BookingForm = ()=> {
     return (
     <div className="form-side">
-      <form style={{ display: 'grid', gap: '20px' }} className="bookingForm" >
-      <label htmlFor="res-custname">First Name</label>
-      <input type="text" id="res-custname" />
+      <form>
+      <label htmlFor="res-custname">First Name:</label>
+      <input type="text" id="res-custname" placeholder="Enter your name.." />
 
-      <label htmlFor="res-email">Email</label>
-      <input type="email" id="res-email" />
+      <label htmlFor="res-email">Email:</label>
+      <input type="email" id="res-email" placeholder="Enter your email.."/>
 
-      <label htmlFor="res-phone-number">First Name</label>
-      <input type="number" id="res-phone-number" />
+      <label htmlFor="res-phone-number">Phone Number:</label>
+      <input type="number" id="res-phone-number" placeholder="Enter your phone number.."/>
 
       <div className="res-col">
-      <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" />
+        <div className="res-date-row">
+          <label htmlFor="res-date">Date of reservation:</label><br></br>
+          <input type="date" id="res-date" />
+        </div>
         
-        <label htmlFor="res-time">Choose time</label>
+        <div className="res-time-row">
+        <label htmlFor="res-time">Time of reservation:</label><br></br>
         <select id="res-time">
           <option>17:00</option>
           <option>18:00</option>
@@ -27,24 +31,30 @@ const BookingForm = ()=> {
           <option>21:00</option>
           <option>22:00</option>
         </select>
+        </div>
+        
       </div>
         
         <div className="res-col">
-        <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+          <div className="res-gst-row">
+            <label htmlFor="guests">Number of guests:</label><br></br>
+            <input type="number" placeholder="1" min="1" max="10" id="guests" />
+          </div>
         
-        <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
-          <option>Birthday</option>
-          <option>Anniversary</option>
-        </select>
+          <div className="res-occ-row">
+            <label htmlFor="occasion">Occasion:</label><br></br>
+            <select id="occasion">
+              <option>Birthday</option>
+              <option>Anniversary</option>
+            </select>
+          </div>
         </div>
 
 
         <label htmlFor="res-msg">Your message:</label>
         <textarea id="res-msg" name="res-msg" rows="4" cols="50"> </textarea>
         
-        <input type="submit" value="BOOK NOW" />
+        <input type="submit" value="BOOK NOW" className="submitBtn" />
       </form>
     </div>
     );
